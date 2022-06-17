15dd6e4c910cf1da13aa56f55e458ee0234d907e
dunleavyjack <dunleavyjack@gmail.com>
2022-06-16 23:05:22 -0400

    Updated custom hook

--- src/hooks/useAnalyzingSongs.ts
+++ src/hooks/useAnalyzingSongs.ts
@@ -1,14 +1,14 @@
 import { useEffect, useState } from 'react';
-import { Songs } from '../types';
+import { Song, Songs } from '../types';
 
 export const useAnalyzingSongs = (songs: Songs) => {
-    const [currentSong, setCurrentSong] = useState<any>({});
+    const [currentSong, setCurrentSong] = useState<Song>(songs[0]);
     const [finishedAnalyzingSongs, setFinishedAnalyzingSongs] =
         useState<boolean>(false);
 
     useEffect(() => {
         // Show user each album for 1.5 seconds
-        songs.forEach((song: any, index: number) => {
+        songs.forEach((song: Song, index: number) => {
             setTimeout(() => {
                 setCurrentSong(song);
             }, index * 150);
