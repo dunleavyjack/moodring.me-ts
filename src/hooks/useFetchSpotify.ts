a77f345d73a2163e1eb8c7339232d1c3d4706ab6
dunleavyjack <dunleavyjack@gmail.com>
2022-06-16 23:05:39 -0400

    Reorganized custom hook logic

--- src/hooks/useFetchSpotify.ts
+++ src/hooks/useFetchSpotify.ts
@@ -1,39 +1,61 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import {
     getUserProfile,
     getRecentSongs,
     getRecentAudioFeatures,
 } from '../api/spotifyAPI';
-import { User, Songs } from '../types';
+import { setRecentSongs, setUser, setAudioFeatures } from '../actions';
+import { User, Songs, AudioFeatures } from '../types';
+import { store } from '../store';
+import {
+    demoUser,
+    demoSongs,
+    demoAudioFeatures,
+} from '../assets/demo/demoSampleData';
 
 export const useFetchSpotify = () => {
-    const [user, setUser] = useState<User | {}>();
-    const [songs, setSongs] = useState<Songs | []>([]);
-    const [audioFeatures, setAudioFeatures] = useState<
-        SpotifyApi.MultipleAudioFeaturesResponse | []
-    >([]);
-
+    const { isDemo } = store.getState().session;
+    const [spotifyRequestsComplete, setSpotifyRequestsComplete] =
+        useState<boolean>(false);
     const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     useEffect(() => {
         const fetchSongsAndUser = async () => {
             try {
-                const userProfile: User | {} = await getUserProfile();
-                setUser(userProfile);
-                const recentSongs: Songs | [] = await getRecentSongs();
-                setSongs(recentSongs);
-                const recentAudioFeatures: any = await getRecentAudioFeatures(
-                    recentSongs
-                );
-                setAudioFeatures(recentAudioFeatures);
+                const userProfile: User | void = await getUserProfile();
+                if (userProfile) {
+                    dispatch(setUser(userProfile));
+                }
+                const recentSongs: Songs | void = await getRecentSongs();
+                if (recentSongs) {
+                    dispatch(setRecentSongs(recentSongs));
+
+                    // Song ids from recent songs are needed to request recent song audio features
+                    const recentAudioFeatures: AudioFeatures | void =
+                        await getRecentAudioFeatures(recentSongs);
+                    if (recentAudioFeatures) {
+                        dispatch(setAudioFeatures(recentAudioFeatures));
+                    }
+                    setSpotifyRequestsComplete(true);
+                }
             } catch (error) {
                 console.error(error);
                 navigate('/error');
             }
         };
-        fetchSongsAndUser();
+
+        const setSampleSongsAndUser = () => {
+            dispatch(setUser(demoUser));
+            dispatch(setRecentSongs(demoSongs));
+            dispatch(setAudioFeatures(demoAudioFeatures));
+            setSpotifyRequestsComplete(true);
+        };
+
+        isDemo ? setSampleSongsAndUser() : fetchSongsAndUser();
     }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-    return { user, songs, audioFeatures };
+    return spotifyRequestsComplete;
 };
