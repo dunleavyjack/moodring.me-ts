5eb25afa5cddf4bdfcc988366d2b7bbadb5f0fa1
dunleavyjack <dunleavyjack@gmail.com>
2022-07-04 14:46:17 -0400

    Updated env variables

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,23 +2,26 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { setDemoStatus } from '../../actions';
 import { useNavigate } from 'react-router-dom';
+import { useViewportWidth } from '../../hooks/useViewportWidth';
 import howAreYouFeelingDesktop from '../../assets/pageAssets/home/howAreYouFeelingDesktop.png';
+import howAreYouFeelingMobile from '../../assets/pageAssets/home/howAreYouFeelingMobile.png';
 import './HomePage.css';
 
 const HomePage: React.FC = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const { isMobile } = useViewportWidth();
     const {
-        REACT_APP_SPOTIFY_CLIENT_ID,
-        REACT_APP_SPOTIFY_AUTHORIZE_URL,
-        REACT_APP_SPOTIFY_REDIRECT_URL,
-        REACT_APP_SPOTIFY_SCOPES,
+        REACT_APP_SPOTIFY_AUTHORIZE_URL: spotifyURL,
+        REACT_APP_SPOTIFY_CLIENT_ID: cliendID,
+        REACT_APP_SPOTIFY_REDIECT_URI: redirectURI,
+        REACT_APP_SPOTIFY_SCOPES: spotifyScopes,
     }: any = process.env;
 
     const handleSpotifyLogin = (): void => {
-        window.location.href = `${REACT_APP_SPOTIFY_AUTHORIZE_URL}?client_id=${REACT_APP_SPOTIFY_CLIENT_ID}&scope=${encodeURIComponent(
-            REACT_APP_SPOTIFY_SCOPES
-        )}&redirect_uri=${REACT_APP_SPOTIFY_REDIRECT_URL}&response_type=token&show_dialog=true`;
+        window.location.href = `${spotifyURL}?client_id=${cliendID}&scope=${encodeURIComponent(
+            spotifyScopes
+        )}&redirect_uri=${redirectURI}/redirect&response_type=token&show_dialog=true`;
     };
 
     const handleDemoLogin = (): void => {
@@ -28,11 +31,19 @@ const HomePage: React.FC = () => {
 
     return (
         <main className="page-content">
-            <img
-                className="home-image"
-                src={howAreYouFeelingDesktop}
-                alt="How are you feeling?"
-            />
+            {isMobile ? (
+                <img
+                    className="home-image"
+                    src={howAreYouFeelingMobile}
+                    alt="How are you feeling?"
+                />
+            ) : (
+                <img
+                    className="home-image"
+                    src={howAreYouFeelingDesktop}
+                    alt="How are you feeling?"
+                />
+            )}
             <section className="home-content">
                 <h1>A mood detector based on your recently played music.</h1>
                 <button
