37467badf0da68729fec36a0204120323bfdf0ef
dunleavyjack <dunleavyjack@gmail.com>
2022-07-04 14:45:32 -0400

    Navbar redesign

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import './Navbar.css';
+
+interface Props {
+    isFullscreen?: boolean;
+}
 
 const handleHomeClick = () => {
     window.location.href = './';
@@ -9,14 +14,21 @@ const handleAboutClick = () => {
     window.location.href = './about';
 };
 
-const Navbar: React.FC = () => {
+const Navbar: React.FC<Props> = ({ isFullscreen = false }) => {
+    const { pathname } = useLocation();
+    const isMoodPage = pathname === '/mood';
+
+    if (isMoodPage && !isFullscreen) {
+        return <></>;
+    }
+
     return (
-        <nav className="header">
-            <h1 className="home" onClick={handleHomeClick}>
+        <nav className={isMoodPage ? 'fullscreen-header' : 'header'}>
+            <h1 className={'home'} onClick={handleHomeClick}>
                 Mood Ring
             </h1>
-            <h2 className="about" onClick={handleAboutClick}>
-                About Us
+            <h2 className={'about'} onClick={handleAboutClick}>
+                About
             </h2>
         </nav>
     );
