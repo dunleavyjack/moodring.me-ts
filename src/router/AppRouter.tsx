514b486608885ea131de5ff708d0ae1a9bc99327
dunleavyjack <dunleavyjack@gmail.com>
2022-07-04 14:47:12 -0400

    Added path for analyzing page

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -3,7 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import '../styles/global.css';
 import Navbar from '../components/Navbar/Navbar';
 import HomePage from '../pages/HomePage/HomePage';
-import YourMoodPage from '../pages/YourMoodPage/YourMoodPage';
+import AnalyzingPage from '../pages/AnalyzingPage/AnalyzingPage';
+import MoodPage from '../pages/MoodPage/MoodPage';
 import AboutPage from '../pages/AboutPage/AboutPage';
 import SpotifyRedirect from '../pages/SpotifyRedirect/SpotifyRedirect';
 import ErrorPage from '../pages/ErrorPage/ErrorPage';
@@ -14,7 +15,8 @@ const AppRouter: React.FC = () => (
             <Navbar />
             <Routes>
                 <Route path="/" element={<HomePage />} />
-                <Route path="/mood" element={<YourMoodPage />} />
+                <Route path="/analyzing" element={<AnalyzingPage />} />
+                <Route path="/mood" element={<MoodPage />} />
                 <Route path="/about" element={<AboutPage />} />
                 <Route path="/redirect" element={<SpotifyRedirect />} />
                 <Route path="/error" element={<ErrorPage />} />
