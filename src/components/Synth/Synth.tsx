7828369e0886a2ecd19250cf08ddcca3d7fc1c29
dunleavyjack <dunleavyjack@gmail.com>
2022-07-05 09:36:02 -0400

    Updated ToneJS component

--- src/components/Synth/Synth.tsx
+++ src/components/Synth/Synth.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import * as Tone from 'tone';
+import { Synth as ToneSynth } from 'tone';
 import synth from '../../assets/synth/synth.svg';
 import './Synth.css';
 
@@ -8,32 +8,23 @@ interface Props {
 }
 
 const Synth: React.FC<Props> = ({ note }) => {
-    const playTone = (musicalNote: string) => {
-        const synth: Tone.Synth<Tone.SynthOptions> =
-            new Tone.Synth().toDestination();
-        const now: number = Tone.now();
+    const playableSynth = new ToneSynth().toDestination();
 
-        // Start note immediately
-        synth.triggerAttack(musicalNote, now);
-        // Finish note after one second
-        synth.triggerRelease(now + 1);
-        Tone.start();
+    const playTone = (): void => {
+        playableSynth.triggerAttackRelease(note, '4n');
     };
 
     return (
         <section className="synth-container">
             <h3>
-                <span>{note} </span>Average Key
+                <span>{note.replace('4', '')} </span>Average Key
             </h3>
-            <img
-                src={synth}
-                alt="Synth"
-                className="synth"
-                onClick={() => playTone(note)}
-            />
-            <button className="synth-button" onClick={() => playTone(note)}>
-                Press to listen to your mood sound
-            </button>
+            <img src={synth} alt="Synth" className="synth" />
+            <div className="synth-button-container">
+                <button className="synth-button" onClick={() => playTone()}>
+                    Press to listen to your mood sound
+                </button>
+            </div>
         </section>
     );
 };
